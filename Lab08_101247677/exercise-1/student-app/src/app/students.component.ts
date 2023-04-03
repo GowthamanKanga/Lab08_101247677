536afbe2e1ce6ffc3ce669f7384a36b27b30737f
GowthamanKanga <83148850+GowthamanKanga@users.noreply.github.com>
2023-04-02 22:53:28 -0400

    Add files via upload

---
+++ Lab08_101247677/exercise-1/student-app/src/app/students.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'students',
+  template: `<h1>{{ getTitle() }} - {{ getCurrentDate() }}</h1>`
+})
+export class StudentsComponent {
+  title = 'My List of Students';
+
+  getTitle() {
+    return this.title;
+  }
+
+  getCurrentDate() {
+    return new Date().toLocaleDateString();
+  }
+}
